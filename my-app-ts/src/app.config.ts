7ae1c9537a70b670e3b7294d08ea82a07bbd25a8
Fat Person <58603432@qq.com>
2022-06-06 14:18:41 +0800

    use f2 lower version demo
    use f2 lower version demo

--- my-app-ts/src/app.config.ts
+++ my-app-ts/src/app.config.ts
@@ -1,8 +1,6 @@
 const pages = ['pages/index/index'];
 const color = '#282c34';
-
-import { AppConfig as AliAppConfig } from 'remax/ali';
-import { AppConfig as ToutiaoAppConfig } from 'remax/toutiao';
+ 
 import { AppConfig as WechatAppConfig } from 'remax/wechat';
 
 export const wechat: WechatAppConfig = {
@@ -10,24 +8,6 @@ export const wechat: WechatAppConfig = {
   window: {
     navigationBarBackgroundColor: color,
     navigationBarTitleText: 'Remax One Wechat',
-  },
-  "usingComponents": {
-    "f2": "@antv/f2-wx"
   }
 };
-
-export const ali: AliAppConfig = {
-  pages,
-  window: {
-    defaultTitle: 'Remax One Ali',
-    titleBarColor: color,
-  },
-};
-
-export const toutiao: ToutiaoAppConfig = {
-  pages,
-  window: {
-    navigationBarTitleText: 'Remax One Toutiao',
-    navigationBarBackgroundColor: color,
-  },
-};
+ 
